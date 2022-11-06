483e10912123dcb9606ac35b55bf12165189bdea
paruljamwal <paruljamwal2000@gmail.com>
2022-11-06 23:48:32 +0530

    functionality

--- script.js
+++ script.js
@@ -6,22 +6,40 @@ document.getElementById("green");
 
 function green(){
     let green = document.getElementById("green");
+    let red = document.getElementById("red");
+
      green.style.backgroundColor="rgb(48, 243, 48)";
      green.style.boxShadow="0px 0px 10px rgb(48, 243, 48), 0px 0px 20px rgb(48, 243, 48)"
-}
+    setTimeout(yellow,2000);
+    red.style.backgroundColor="black";
+    red.style.boxShadow="none";
 
-green()
+    }
+
 
 function yellow(){
-    let green = document.getElementById("yellow");
-     green.style.backgroundColor="yellow";
-     green.style.boxShadow=" 0px 0px 10px yellow,  0px 0px 20px yellow "
-}
+    let yellow = document.getElementById("yellow");
+    let green = document.getElementById("green");
+
+     yellow.style.backgroundColor="yellow";
+     yellow.style.boxShadow=" 0px 0px 10px yellow,  0px 0px 20px yellow "
+     green.style.backgroundColor="black";
+     green.style.boxShadow="none";
+     setTimeout(red,2000);
+    
+    }
 
 
 function red(){
-    let green = document.getElementById("red");
-     green.style.backgroundColor="red";
-     green.style.boxShadow="0px 0px 10px red,0px 0px 20px red"
-}
+    let red = document.getElementById("red");
+    let yellow = document.getElementById("yellow");
 
+     red.style.backgroundColor="red";
+     red.style.boxShadow="0px 0px 10px red,0px 0px 20px red"
+     yellow.style.backgroundColor="black";
+     yellow.style.boxShadow="none";
+     setTimeout(green,2000);
+
+    }
+
+green()
